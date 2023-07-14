6d3491b2e19884cfb8c53f45b59e6e0bcbca6719
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-07-14 11:53:10 -0400

    corrected User Table

--- Models/User.js
+++ Models/User.js
@@ -5,31 +5,35 @@ const db = require('../db/connection');
 class User extends Model { }
 
 User.init({
-
-    first_name: {
-        type: DataTypes.STRING,
-        unique: false,
-        allowNull: false,
-        validate: {
-            min: 2
-        }
-      },
-
-      last_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            min: 2
-        }
-      },
-
-
+  first_name: {
+    type: DataTypes.STRING,
+    unique: false,
+    allowNull: false,
+    validate: {
+      min: 2
+    }
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      min: 2
+    }
+  },
   email: {
     type: DataTypes.STRING,
     unique: true,
     allowNull: false,
     validate: {
       isEmail: true
+    }
+  },
+  user_name: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      min: 5
     }
   },
   password: {
@@ -41,18 +45,6 @@ User.init({
 }, {
   sequelize: db,
   modelName: 'user',
-  hooks: {
-    async beforeCreate(user) {
-      const hashPassword = await hash(user.password, 10);
-      user.password = hashPassword;
-    }
-  }
 });
-
-User.prototype.validatePass = async function (formPassword) {
-  const isValid = await compare(formPassword, this.password);
-
-  return isValid;
-}
 
 module.exports = User;
