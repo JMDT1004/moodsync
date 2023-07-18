1e6677178b3ec2e78fb0c32fb6e5b1f5ff407af4
JT <raikiri24@gmail.com>
2023-07-18 14:06:25 -0400

    update localhost

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('user_db', 'root', '', {
+const sequelize = new Sequelize('user_db', 'root', 'pass', {
   host: '127.0.0.1',
   dialect: 'mysql'
 });
